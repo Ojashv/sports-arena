7ee6b451742509ba2ca2130278e279730d6d5a27
Ojashv <151911074+Ojashv@users.noreply.github.com>
2026-02-06 11:06:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({
-    behavior: "smooth"
-  });
+  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
 }
 
 function showEquipment(sport) {
-  let all = document.querySelectorAll(".equipment-list");
+  document.getElementById("clickSound").play();
 
-  all.forEach(item => {
-    item.classList.remove("active");
-    item.style.display = "none";
-  });
+  let items = document.querySelectorAll(".equipment");
+  items.forEach(i => i.style.display = "none");
 
   let selected = document.getElementById(sport);
   selected.style.display = "block";
 
-  setTimeout(() => {
-    selected.classList.add("active");
-  }, 50);
-
-  document.getElementById("equipment").scrollIntoView({
-    behavior: "smooth"
-  });
+  document.getElementById("equipment").scrollIntoView({ behavior: "smooth" });
 }
