88a582581f38b39d4802ead763fc6717105a06b4
Ojashv <151911074+Ojashv@users.noreply.github.com>
2026-02-06 10:52:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function scrollToSection(id) {
-  const section = document.getElementById(id);
-  if (section) {
-    section.scrollIntoView({ behavior: "smooth" });
-  }
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
 }
 
-function submitForm(event) {
-  event.preventDefault();
-  alert("Thank you! Your message has been sent.");
+function showEquipment(sport) {
+  let lists = document.querySelectorAll(".equipment-list");
+  lists.forEach(list => list.style.display = "none");
+
+  document.getElementById(sport).style.display = "block";
 }
